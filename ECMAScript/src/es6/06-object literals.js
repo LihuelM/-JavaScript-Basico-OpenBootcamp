d91fad6070f160f810e0438ad3c764148f020b9f
Lihuel <muracciole.al@gmail.com>
2022-12-09 11:30:56 +0800

    res, spread, object, promise, module

---
+++ ECMAScript/src/es6/06-object literals.js
@@ -0,0 +1,13 @@
+// Los objetos literales consiste en crear objetos a partir de variables sin repetir el nombre. 
+
+const nombre = "Andres"
+const edad = 23
+const esteEsUnID = 1
+
+const objeto = {
+    nombre, 
+    edad,
+    id: esteEsUnID
+}
+
+objeto // { nombre: 'Andres', edad: 23, id: 1 }
---
+++ ECMAScript/src/es6/07-promise.js
@@ -0,0 +1,16 @@
+// Una promesa es una forma de manejar el asincronismo en JavaScript y se representa como un objeto que puede generar un valor único a futuro, que tiene dos estados, o está resuelta o incluye una razón por la cual no ha sido resuelta la solicitud. El argumento de la clase Promise es una función que recibe dos parámetros: *resolve: cuando la promesa es resuelta. *reject: cuando la promesa es rechazada.
+
+const promesa = () => {
+    return new Promise((resolve, reject) => {
+        if (something) {
+        //true o false
+        resolve("Se ha resuelto la promesa")
+        } else {
+        reject("Se ha rechazado la promesa")
+        }
+    })
+}
+
+promesa()
+    .then(respuesta => console.log(respuesta)) //En caso que se ejecute resolve
+    .catch(error => console.log(error)) //En caso que se ejecute reject
